e44c9ee5a87d53e02a6a68ed1ff28b5c14c808c5
rishu.s <601rishu@gmail.com>
2017-08-12 19:03:47 +0530

    First Commit.

---
+++ Connect Attribute.py
@@ -0,0 +1,16 @@
+'''
+grp = Select Animated Object or CTRL.
+CtrlName = Fingers_L.Relax (Put ctrl name which have a attribute)
+'''
+
+import pymel.core as pm
+def connectAttribute():
+	grp = pm.ls(sl=True)
+	pm.delete(grp,sc=True)
+	CtrlName = "nurbsCircle1.Test"
+	animCurve = pm.findKeyframe(grp[0],c=True)
+	for i in range(len(animCurve)):
+		connect = pm.connectAttr(CtrlName,animCurve[i]+".input")
+	print "animCurve Connected...",
+		
+connectAttribute()
